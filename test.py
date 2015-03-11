e8db1e48e008ec216cbeeecbe7da118687d12cd8
John <kohler.j@husky.neu.edut>
2015-03-11 12:58:55 -0400

    MORE TESTS!!!

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import xlrd
 import unittest
-from workbook import Workbook
+from workbook_reader import WorkbookReader
+from workbook_writer import WorkbookWriter, SlideMetricWriter, LookzoneWriter
 
 TEST_WORKBOOK_1 = 'sample_inputs/191144- ATT 2 NEG.xlsx'
 TEST_WORKBOOK_2 = 'sample_inputs/191150- ATT 2 NEG.xlsx'
@@ -10,13 +11,31 @@ TEST_WORKBOOK_3 = 'test_xls/test_sheet_count.xls'
 class TestWorkbookMethods(unittest.TestCase):
 
     def setUp(self):
-        self.workbook1 = Workbook(TEST_WORKBOOK_1)
-        self.workbook2 = Workbook(TEST_WORKBOOK_2)
-        self.workbook3 = Workbook(TEST_WORKBOOK_3)
+        self.workbook1 = WorkbookReader(TEST_WORKBOOK_1)
+        self.workbook2 = WorkbookReader(TEST_WORKBOOK_2)
+        self.workbook3 = WorkbookReader(TEST_WORKBOOK_3)
 
         self.wb1_sheets = self.workbook1.get_sheets()
         self.wb2_sheets = self.workbook2.get_sheets()
         self.wb3_sheets = self.workbook3.get_sheets()
+
+    def sheets_equal(self, sheet1, sheet2):
+        # Internal method:  Determine if the two given sheets are equivalent
+        for row in xrange(sheet1.nrows):
+            for col in xrange(sheet1.ncols):
+                if sheet1.cell_value(row,col) != sheet2.cell_value(row,col):
+                    return False
+        return True
+
+    def books_equal(self,book1,book2):
+        # Internal method: Determine if two workbooks are equal
+        book1_sheets = book1.sheets() # list of sheet
+        book2_sheets = book2.sheets()
+
+        for i in xrange(len(book1_sheets)):
+            if not self.sheets_equal(book1_sheets[i],book2_sheets[i]):
+                return False
+        return True
 
     def test_sheets_returned(self):
         # make sure the number of STAT sheets in the workbook is correct.
@@ -42,14 +61,24 @@ class TestWorkbookMethods(unittest.TestCase):
         for sheet in self.wb2_sheets:
             self.assertTrue(self.workbook2.get_lookzones(sheet) > 0)
 
-    def test_grab_attributes(self):
-        # make sure the number of attributes is correct
+    def test_get_slide_metrics(self):
+        # make sure slidemetrics are returned from the workbook for each sheet.
+        checked_attr = "Percent time tracked" # I believe they all have this
+        for sheet in self.wb1_sheets:
+            self.assertTrue(self.workbook1.get_slidemetrics(sheet).has_attribute(checked_attr))
+        for sheet in self.wb2_sheets:
+            self.assertTrue(self.workbook2.get_slidemetrics(sheet).has_attribute(checked_attr))
+
+    def test_get_attributes(self):
+        # Test that the hash includes two values: slide and lookzone. 
+        # Verify that each of these contains a set with the correct
+        # number of values
         hsh_len = 2
         slide_len = 38
         lookzone_len = 120
 
-        attrs1 = self.workbook1.grab_attributes()
-        attrs2 = self.workbook2.grab_attributes()
+        attrs1 = self.workbook1.get_attributes()
+        attrs2 = self.workbook2.get_attributes()
         self.assertEqual(len(attrs1),hsh_len)
         self.assertEqual(len(attrs2),hsh_len)
         self.assertEqual(len(attrs1["slide"]), slide_len)
@@ -65,6 +94,30 @@ class TestWorkbookMethods(unittest.TestCase):
         self.assertEqual(workbook1_id, '191144')
         self.assertEqual(workbook2_id, '191150')
 
+    def test_write_lookzones(self):
+        # Test the output of LookZoneWriter to that of an
+        # Expected output file
+        output_path = "./Test_Files/TestLookzoneOutput.xls"
+        expected_output_path = "./Test_Files/ExpectedLookzoneOutput.xls"
+        lookzone_writer = LookzoneWriter([self.workbook1],output_path,["Width", "Appears"])
+        lookzone_writer.write_first_reader()
+        expected_book = xlrd.open_workbook(expected_output_path)
+        resulting_book = xlrd.open_workbook(output_path)
+
+        self.assertTrue(self.books_equal(expected_book,resulting_book))
+
+    def test_write_slide_metrics(self):
+        # Test the output of SlideMetricWriter to that of an
+        # Expected output file
+        output_path = "./Test_Files/TestSlideMetricOutput.xls"
+        expected_output_path = "./Test_Files/ExpectedSlideMetricOutput.xls"
+        test_attrs = ["Percent time tracked", "Average pupil area", "Number of fixations"]
+        slide_writer = SlideMetricWriter([self.workbook1],output_path,test_attrs)
+        slide_writer.write_first_reader()
+        expected_book = xlrd.open_workbook(expected_output_path)
+        resulting_book = xlrd.open_workbook(output_path)
+
+        self.assertTrue(self.books_equal(expected_book,resulting_book))
 
 
 if __name__ == '__main__':
