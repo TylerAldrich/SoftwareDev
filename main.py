41a9d315beab5d74ff8c2c01b1b3282b14df1974
TylerAldrich <aldrich.ty@husky.neu.edu>
2015-03-29 15:47:28 -0400

    updated workbook_writer to handle writing multiple reader objects and updated main.py for testing

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ from configuration import Configuration
 
 #Checks that the command line contains the necessary arguments and throws an error if not
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print "Must give this program a workbook to open, a filename to save as Look Zones, and a filename to save as Slide Metrics"
         sys.exit(1)
 
     #Sets the command line arguments
     workbook_file = sys.argv[1]
-    lookzone_output  = sys.argv[2]
-    slide_metric_output = sys.argv[3]
+    workbook_file2 = sys.argv[2]
+    lookzone_output  = sys.argv[3]
+    slide_metric_output = sys.argv[4]
 
     # placeholder attributes
     lookzone_attrs = ["Width", "Appears", "Gazepoint count"]
@@ -24,16 +25,14 @@ if __name__ == '__main__':
 
     # create reader(s)
     workbook_reader = WorkbookReader(workbook_file)
+    workbook_reader2 = WorkbookReader(workbook_file2)
 
     # create writers
-    lookzone_writer = LookzoneWriter([workbook_reader], lookzone_output, lookzone_attrs)
-    slide_metric_writer = SlideMetricWriter([workbook_reader], slide_metric_output, slide_attrs)
+    lookzone_writer = LookzoneWriter([workbook_reader, workbook_reader2], lookzone_output, lookzone_attrs)
+    slide_metric_writer = SlideMetricWriter([workbook_reader, workbook_reader2], slide_metric_output, slide_attrs)
 
     #Writes the two workbooks and prints a success message for each file created
     for writer in [lookzone_writer, slide_metric_writer]:
-        writer.write_first_reader()
+        writer.write_readers()
         print "File created: " + writer.output
-
-
-
 
