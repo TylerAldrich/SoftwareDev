b958ed5a25779640b9ae7a5e7fb255bcf8da0cb3
Michael Alex Ravert <ravert1@batsugun.ccs.neu.edu>
2015-03-31 21:29:58 -0400

    Adjusting load screen to select multiple files

--- Views/loadFile.py
+++ Views/loadFile.py
@@ -8,46 +8,49 @@ from navigation import NavigationWidget
 
 # Widget that has user browse for an input file
 class LoadFileWidget(QtGui.QWidget):
-	procNext = QtCore.pyqtSignal()
+  procNext = QtCore.pyqtSignal()
 
-	def __init__(self, window):
-		QtGui.QWidget.__init__(self)
-		self.window = window
-		self.initUI()
+  def __init__(self, window):
+    QtGui.QWidget.__init__(self)
+    self.window = window
+    self.initUI()
 
-	def initUI(self):
-		# Create initial vertical layout
-		layout = QtGui.QVBoxLayout(self)
-		layout.setAlignment(QtCore.Qt.AlignTop)
+  def initUI(self):
+    # Create initial vertical layout
+    layout = QtGui.QVBoxLayout(self)
+    layout.setAlignment(QtCore.Qt.AlignTop)
 
-		titleLabel = QtGui.QLabel('Upload Excel File', self)
-		subtitleLabel = QtGui.QLabel('Click browse to select an experiment to upload', self)
+    titleLabel = QtGui.QLabel('Upload Excel File', self)
+    subtitleLabel = QtGui.QLabel('Click browse to select an experiment to upload', self)
 
-		# Add two labels to layout
-		layout.addWidget(titleLabel)
-		layout.addWidget(subtitleLabel)
+    # Add two labels to layout
+    layout.addWidget(titleLabel)
+    layout.addWidget(subtitleLabel)
 
-		# Horizontal layout is for the text box and browse button
-		browseFileLayout = QtGui.QHBoxLayout(self)
+    # Horizontal layout is for the text box and browse button
+    browseFileLayout = QtGui.QHBoxLayout(self)
 
-		# Init text edit box for file path and browse button to
-		# find the file.  Set browse button on click to selectFile function
-		self.fileTextEdit = QtGui.QLineEdit()
-		browseButton = QtGui.QPushButton('Browse')
-		browseFileLayout.addWidget(self.fileTextEdit)
-		browseFileLayout.addWidget(browseButton)
-		browseButton.clicked.connect(self.selectFile)
+    # Init text edit box for file path and browse button to
+    # find the file.  Set browse button on click to selectFile function
+    self.fileTextEdit = QtGui.QLineEdit()
+    browseButton = QtGui.QPushButton('Browse')
+    browseFileLayout.addWidget(self.fileTextEdit)
+    browseFileLayout.addWidget(browseButton)
+    browseButton.clicked.connect(self.selectFile)
 
-		# Add horizontal layout to overall layout
-		layout.addLayout(browseFileLayout)
-		navigation = NavigationWidget(self.window, None, self.switchViews)
-		layout.addWidget(navigation)
+    # Add horizontal layout to overall layout
+    layout.addLayout(browseFileLayout)
+    navigation = NavigationWidget(self.window, None, self.switchViews)
+    layout.addWidget(navigation)
 
-	# go to next view to select data attributes
-	def switchViews(self):
-		fileName = self.fileTextEdit.text()
-		self.window.showLoadConfigView(fileName)
+  # go to next view to select data attributes
+  def switchViews(self):
+    fileName = self.fileTextEdit.text()
+    self.window.showLoadConfigView(fileName)
 
-	# open a file dialog to pick an xlsx input file
-	def selectFile(self):
-		self.fileTextEdit.setText(QtGui.QFileDialog.getOpenFileName())
+  # open a file dialog to pick an xlsx input file
+  def selectFile(self):
+    select_dialog = QtGui.QFileDialog(self)
+    select_dialog.setFileMode(QtGui.QFileDialog.AnyFile)
+    file_names = map(str, select_dialog.getOpenFileNames())
+    self.fileTextEdit.setText(str(file_names))
