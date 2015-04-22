fd1c1147612d8ad97466355ea74686e66db24bf3
John <kohler.j@husky.neu.edut>
2015-04-22 14:27:36 -0400

    changed a thing

--- ipatch_exception.py
+++ ipatch_exception.py
@@ -1,6 +1,5 @@
 # This class simply acts as a wrapper for exceptions thrown 
 # from the Workbook Readers and Writers so the front end
 # application can properly identify and react to errors
-""" Custom Wrapper by iPatch errors thrown from Back-End """
 class IPatchException(Exception):
     pass
